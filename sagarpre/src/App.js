2a538e6ef58acf8ed00dee1e3f3a043603f60861
Sagar1917118 <112215153@cse.iiitp.ac.in>
2023-10-26 22:55:05 +0530

    UI

---
+++ sagarpre/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SingUpForm from './Components/SignUpForm';
+function App() {
+  return (
+    <div className="App">
+      <SingUpForm/>
+    </div>
+  );
+}
+
+export default App;
